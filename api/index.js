eb911c68cb9be5fed6fbcda3faf08d0282a900a3
- <->
2025-09-25 23:58:28 +0500

    2

--- api/index.js
+++ api/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 module.exports = app
 app.get('/', (req, res) => {
-    res.send("Express App Responded");
+    res.send("Express App Responded!!!");
 })
 
-app.listen(process.env.PORT || 3000);
+//app.listen(process.env.PORT || 3000);
 module.exports = app
 
